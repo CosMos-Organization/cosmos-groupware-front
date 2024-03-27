8c3f0dbd1b7598c5dcaeacbb82adf638d037a24c
BaikJinHyeock <me335097@gmail.com>
2024-03-28 00:17:26 +0900

    240328 board 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
+import Board from "./pages/Board";
 import './SCSS/base/reset.scss'
 import './SCSS/base/font.scss'
 import Admin_Employee from "./pages/Admin_Employee";
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/board" element={<Board />}></Route>
         <Route path="/admin/employee" element={<Admin_Employee />}></Route>
         <Route path="/admin/org" element={<Admin_Organization />}></Route>
         <Route path="/calendar" element={<React_Calendar />}></Route>
